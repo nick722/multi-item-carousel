4b2c15714797a979e59d393fc1a48942221f9666
Nikolai <nick22783@gmail.com>
2024-06-05 09:47:43 +0300

    23.32  move slider by clicking next

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const next = document.querySelector(".next");
+const card = document.querySelector(".carousel-container");
+console.log("card", card);
+
 const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+const track = document.querySelector(".track");
+const carouselWidth = document.querySelector(".carousel-container").offsetWidth;
+const cardWidth = document.querySelector(".card-container").offsetWidth;
+
+next.addEventListener("click", () => {
+  track.style.transform = `translateX(-${cardWidth}px)`;
+});
